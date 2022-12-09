44ffb475f7284ce194f8768561861a8553cd4351
nickel <sandmanmufmaf@gmail.com>
2022-12-09 09:46:49 -0600

    I FINALLY GOT IT!!!

--- 2022/day7.java
+++ 2022/day7.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.stream.*;
 import java.io.*;
 
-public class day7 {
+public class day7_1 {
 
 	private static class Data implements Comparable<Data> {
 		public static final Data ROOT_DIRECTORY = new Data("", new HashSet<Data>(), "/", 0, true);
@@ -50,27 +50,39 @@ public class day7 {
 		}
 
 		public String toString() {
-			return String.format("%s%s%s = %d", path, name, isFolder ? "/" : "", isFolder ? -1 : size);
+			return String.format("%s%s%s = %d%s", path, name, 
+				isFolder ? "/" : "", isFolder ? -1 : size, isFolder ? " " + files : "");
 		}
 
 		public static Data locateDirectory(final Stack<String> dir) {
 			Queue<String> path = new LinkedList<String>(dir);
 			path.remove(); // starting at root
 			Data current = ROOT_DIRECTORY;
-			while (!path.isEmpty()) {
+			// System.out.println("Start: " + current);
+			while (current != null && !path.isEmpty()) {
 				String next_name = path.remove();
-				Data next = current.search(next_name); 
+				Data next = current.search(next_name);
+				// System.out.println(next == null ? "Not found." : "FOUND " + next);
+				current = next;
 			}
+			// System.out.println("End: " + current);
 			return current;
 		}
-	}
 
-	private static TreeSet<Data> file_system;
-	private static void compileFileSystem(Data current) {
-		file_system.add(current);
-		if (current.isFolder)
-			for (Data data: current.files)
-				compileFileSystem(data);
+		private static TreeSet<Data> file_system;
+		private static void compileFileSystem() {
+			file_system = new TreeSet<>();
+			compileFileSystemRecur(ROOT_DIRECTORY);
+			for (Data data: file_system)
+				data.getSize();
+		}
+
+		private static void compileFileSystemRecur(Data current) {
+			file_system.add(current);
+			if (current.isFolder)
+				for (Data data: current.files)
+					compileFileSystemRecur(data);
+		}
 	}
 
 	private static String stringifyPath(final Stack<String> path) {
@@ -78,7 +90,7 @@ public class day7 {
 	}
 
 	public static void main(String[] args) throws IOException {	
-		Scanner in = new Scanner(new File("../testin"));
+		Scanner in = new Scanner(new File("input"));
 		Stack<String> cwd = new Stack();
 
 		while (in.hasNextLine()) {
@@ -103,6 +115,7 @@ public class day7 {
 					}
 
 					case "ls": {
+						// System.out.println("Searching: " + stringifyPath(cwd));
 						Data folderLocation = Data.locateDirectory(cwd);
 
 						in.hasNext(".*");
@@ -110,7 +123,7 @@ public class day7 {
 						while (in.hasNextLine() && !file[0].equals("$")) {
 							file = in.match().group(0).trim().split(" ");
 
-							System.out.println(Arrays.toString(file));
+							// System.out.println(Arrays.toString(file));
 
 							if (file[0].equals("$"))
 								continue;
@@ -124,22 +137,33 @@ public class day7 {
 									file[0].equals("dir") ? -1 : Long.parseLong(file[0]), file[0].equals("dir"))
 							);
 						}
+
+						// System.out.println(folderLocation);
 					}
 
 				}
 			}
 		}
 
-		file_system = new TreeSet<Data>();
-		compileFileSystem(Data.ROOT_DIRECTORY);
+		Data.compileFileSystem();
 
-		// long total = 0;
-		// for (Data data: file_system)
-		// 	if (data.isFolder && data.getSize() <= 100000)
-		// 		total += data.size;
-		// System.out.println(total);
+		// for (Data data: Data.file_system)
+		// 	System.out.println(data.path + "/" + data.name + ": " + (data.files.isEmpty() ? data.size : data.files));
 
-		for (Data data: file_system)
-			System.out.println(data.path + "/" + data.name + ": " + data.files);
+		ArrayList<Data> folders = new ArrayList<Data>();
+		for (Data data: Data.file_system)
+			if (data.isFolder)
+				folders.add(data);
+
+		for (Data data: folders)
+			System.out.println(data.name + " " + data.size);
+
+		long best_size = Long.MAX_VALUE;
+		long required = 30000000 - (70000000 - Data.ROOT_DIRECTORY.size);
+		// System.out.println(required);
+		for (Data folder: folders) 
+			if (folder.size >= required && folder.size < best_size)
+				best_size = folder.size;
+		System.out.println("Best: " + best_size);
 	}
 }
